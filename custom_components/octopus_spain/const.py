c1a606795a37c12eef91e8f920b2beb7912177f7
Miguel Ángel López Vicente <miguel.angel.lopez.vicente@gmail.com>
2026-06-29 10:38:33 +0200

    Refactor tariff period constants to use strings instead of tuples

--- custom_components/octopus_spain/const.py
+++ custom_components/octopus_spain/const.py
@@ -7,6 +7,6 @@ CONF_PASSWORD = "password"
 
 UPDATE_INTERVAL = 2  # Hours
 
-PERIOD_PEAK_WITH_TAXES = ("peak_with_taxes",)
-PERIOD_STANDARD_WITH_TAXES = ("standard_with_taxes",)
-PERIOD_VALLEY_WITH_TAXES = ("valley_with_taxes",)
+PERIOD_PEAK_WITH_TAXES = "peak_with_taxes"
+PERIOD_STANDARD_WITH_TAXES = "standard_with_taxes"
+PERIOD_VALLEY_WITH_TAXES = "valley_with_taxes"
